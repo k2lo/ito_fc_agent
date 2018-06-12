344d3ace99b77941008a5ed2867d4d800eb78ed7
Karol Onaszkiewicz <karolonasz@gmail.com>
2018-06-12 23:29:05 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ito_fc_agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
